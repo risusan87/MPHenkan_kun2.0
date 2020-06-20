29f4f374bfd8a457ff71744a259aa33a94e654ee
risusan87 <risusan87@localhost>
2020-06-20 08:19:32 -0400

    Mostly done:

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/Tag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/Tag.java
@@ -1,9 +1,12 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
+import java.util.List;
+import java.util.function.Function;
+
 public abstract class Tag {
 	
 	public String Tag_name;
-	public enum type {
+	protected enum type {
 		END, BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, BYTE_ARRAY,
 		STRING, LIST, COMPOUND, INT_ARRAY, LONG_ARRAY
 	}
@@ -18,8 +21,8 @@ public abstract class Tag {
 		this.Type = this.setType();
 	}
 	
-	public abstract type setType();
-	public abstract byte[] toByteArray();
+	protected abstract type setType();
+	protected abstract Function<Tag, byte[]> settoByteArrayFunction();
 	
 	protected static interface ITag {
 		public void addTag(Tag par1tag);
@@ -27,10 +30,18 @@ public abstract class Tag {
 		public void replaceTag(Tag par1src, Tag par2dst);
 	}
 	
-	protected static interface IByteTag {
-		public byte[] toByteTag(int par1size);
+	/**
+	 * Converts this tag into unzipped NBT byte form.
+	 * @return byte array of NBT tag
+	 */
+	public byte[] toByteArray() {
+		return this.settoByteArrayFunction().apply(this);
 	}
 	
+	/**
+	 * Conditions are if given object is an instance of {@code Tag}, their {@code Tag.type} matches,
+	 * and their {@code Tag.Tag_name} matches.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Tag))
