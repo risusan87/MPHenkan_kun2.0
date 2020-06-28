53175d2a5224926a619a4f0706a3abc14e2fbf05
risusan87 <risusan87@localhost>
2020-06-28 17:16:24 -0400

    FloatTag completed

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/FloatTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/FloatTag.java
@@ -1,9 +1,19 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
+import java.nio.ByteBuffer;
+import java.util.function.Function;
+
+/**
+ * One of nbt tags. Holds float
+ * @author risusan87
+ */
 public class FloatTag extends Tag {
 
-	public FloatTag(String par1name) {
+	private float f;
+	
+	public FloatTag(String par1name, float par2setfloat) {
 		super(par1name);
+		this.f = par2setfloat;
 	}
 
 	@Override
@@ -12,8 +22,20 @@ public class FloatTag extends Tag {
 	}
 
 	@Override
-	public byte[] toByteArray() {
-		return null;
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return tag -> {
+			ByteBuffer nbt = ByteBuffer.allocate(tag.getAllocatedByteSize());
+			nbt.put((byte)0x05)
+			.put(StringTag.toNBTByteTag(tag.Tag_name))
+			.putFloat(this.f);
+			return nbt.array();
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Float tagComponent() {
+		return this.f;
 	}
 
 }
