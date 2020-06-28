215e7c900f637a66d7fd331fa8a37ed2de88c0af
risusan87 <risusan87@localhost>
2020-06-28 16:20:21 -0400

    IntArrayTag completed

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/IntArrayTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/IntArrayTag.java
@@ -1,19 +1,57 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
-public class IntArrayTag extends Tag {
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
-	public IntArrayTag(String par1name) {
+/**
+ * One of nbt tags. Holds Int array
+ * @author risusan87
+ */
+public class IntArrayTag extends Tag implements Tag.TagArray {
+	
+	private List<Integer> iarray;
+	
+	public IntArrayTag(String par1name, int[] par2array) {
 		super(par1name);
+		this.iarray = new ArrayList<Integer>();
+		for (int i : par2array)
+			this.iarray.add(i);
 	}
 
 	@Override
 	public type setType() {
-		return null;
+		return type.INT_ARRAY;
 	}
 
 	@Override
-	public byte[] toByteArray() {
-		return null;
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return tag -> {
+			ByteBuffer nbt = ByteBuffer.allocate(tag.getAllocatedByteSize());
+			nbt.put((byte)0x0B)
+			.put(StringTag.toNBTByteTag(tag.Tag_name))
+			.putInt(iarray.size());
+			for (int i : this.iarray)
+				nbt.putInt(i);
+			return nbt.array();
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public synchronized List<Integer> tagComponent() {
+		return this.iarray;
+	}
+
+	@Override
+	public int arraySize() {
+		return this.iarray.size();
+	}
+
+	@Override
+	public int primitiveSize() {
+		return 4;
 	}
 
 }
