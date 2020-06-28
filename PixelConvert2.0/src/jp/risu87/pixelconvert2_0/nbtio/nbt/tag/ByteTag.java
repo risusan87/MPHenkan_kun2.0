f701e529ce997688344f4ea548b2047ef2a3712a
risusan87 <risusan87@localhost>
2020-06-28 16:53:58 -0400

    ByteTag completed

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ByteTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ByteTag.java
@@ -1,9 +1,19 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
+import java.nio.ByteBuffer;
+import java.util.function.Function;
+
+/**
+ * One of nbt tags. Holds Byte
+ * @author risusan87
+ */
 public class ByteTag extends Tag {
 
-	public ByteTag(String par1name) {
+	private byte b;
+	
+	public ByteTag(String par1name, byte par2setbyte) {
 		super(par1name);
+		this.b = par2setbyte;
 	}
 
 	@Override
@@ -12,8 +22,20 @@ public class ByteTag extends Tag {
 	}
 
 	@Override
-	public byte[] toByteArray() {
-		return null;
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return tag -> {
+			ByteBuffer nbt = ByteBuffer.allocate(tag.getAllocatedByteSize());
+			nbt.put((byte)0x01)
+			.put(StringTag.toNBTByteTag(tag.Tag_name))
+			.put(b);
+			return nbt.array();
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public synchronized Byte tagComponent() {
+		return b;
 	}
 	
 }
