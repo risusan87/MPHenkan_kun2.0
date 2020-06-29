3a7b50a682385a62ef77ec6973021a227954350d
risusan87 <risusan87@localhost>
2020-06-29 14:04:36 -0400

    Finishing off ShortTag class
    Started from nothing to done

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ShortTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ShortTag.java
@@ -1,24 +1,57 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
+import java.nio.ByteBuffer;
+import java.util.function.Function;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
+ * One of nbt tags. Holds short
  * 
- * tagID:02
  * @author risusan87
  */
 public class ShortTag extends Tag {
-
-	public ShortTag(String par1name) {
+	
+	private short s;
+	/**
+	 * Creates new tag of short.
+	 * give null as its name to declare as use for list
+	 * 
+	 * @param par1name - tag name
+	 * @param par2setshort - short to be set
+	 */
+	public ShortTag(@Nullable String par1name, @Nonnull short par2setshort) {
 		super(par1name);
+		this.s = par2setshort;
 	}
 
 	@Override
-	public type setType() {
-		return type.SHORT;
+	public tagID setType() {
+		return tagID.SHORT;
 	}
 
 	@Override
-	public byte[] toByteArray() {
-		return null;
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return tag -> {
+			ByteBuffer nbt = ByteBuffer.allocate(getCorrespondedAllocatedByteSize());
+			if (tag.Tag_name != null)
+				nbt.put(tag.getTagID())
+				.put(StringTag.toNBTByteTag(tag.Tag_name));
+			nbt.putShort((short)tag.tagComponent());
+			return nbt.array();
+		};
+	}
+
+	@Override
+	protected byte getTagID() {
+		return (byte)0x02;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Short tagComponent() {
+		return this.s;
 	}
 
 }
