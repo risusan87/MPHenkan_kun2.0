a070b4da22acb0fda9cde8542abebd9aa9905869
risusan87 <risusan87@localhost>
2020-06-29 13:03:35 -0400

    Small fix in FloatTag class
    - nullable and nonnull annotations added to param in the constructor

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/FloatTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/FloatTag.java
@@ -3,8 +3,12 @@ package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 import java.nio.ByteBuffer;
 import java.util.function.Function;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * One of nbt tags. Holds float
+ * 
  * @author risusan87
  */
 public class FloatTag extends Tag {
@@ -16,25 +20,26 @@ public class FloatTag extends Tag {
 	 * give null as its name to declare as use for list
 	 * 
 	 * @param par1name - tag name
-	 * @param par2setbyte - byte to be set
+	 * @param par2setbyte - float to be set
 	 */
-	public FloatTag(String par1name, float par2setfloat) {
+	public FloatTag(@Nullable String par1name, @Nonnull float par2setfloat) {
 		super(par1name);
 		this.f = par2setfloat;
 	}
 
 	@Override
-	public type setType() {
-		return type.FLOAT;
+	public tagID setType() {
+		return tagID.FLOAT;
 	}
 
 	@Override
 	protected Function<Tag, byte[]> _toByteArrayFunction() {
 		return tag -> {
-			ByteBuffer nbt = ByteBuffer.allocate(tag.getAllocatedByteSize());
-			nbt.put((byte)0x05)
-			.put(StringTag.toNBTByteTag(tag.Tag_name))
-			.putFloat(this.f);
+			ByteBuffer nbt = ByteBuffer.allocate(tag.getCorrespondedAllocatedByteSize());
+			if (tag.Tag_name != null)
+				nbt.put(tag.getTagID())
+				.put(StringTag.toNBTByteTag(tag.Tag_name));
+			nbt.putFloat((float)tag.tagComponent());
 			return nbt.array();
 		};
 	}
