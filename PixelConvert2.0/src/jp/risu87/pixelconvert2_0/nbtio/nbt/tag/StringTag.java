1ae1d3e5a2d641733404360647beed85744afe90
risusan87 <risusan87@localhost>
2020-06-29 14:28:07 -0400

    Finishing off StringTag class
    - Added explanation to its constructor and the class
    
    - Nullable and nonnull annotations put on the params in constructor
    
    - Enum variable name changed: type -> tagID
    
    - Removed unnecessary overrided method
    
    - getTagID() done
    
    - _toByteArrayFunction() done
    
    - Tag is now null name friendly

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/StringTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/StringTag.java
@@ -1,19 +1,50 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
-public class StringTag extends Tag {
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
 
-	public StringTag(String par1name) {
+public class StringTag extends Tag {
+	
+	private String str;
+	
+	public StringTag(String par1name, String par2setstr) {
 		super(par1name);
+		this.str = par2setstr;
+	}
+	
+	public static final byte[] toNBTByteTag(String par1str) {
+		ByteBuffer strtag = ByteBuffer.allocate(2 + par1str.length());
+		strtag.putShort((short)par1str.length());
+		strtag.put(par1str.getBytes(StandardCharsets.UTF_8));
+		return strtag.array();
 	}
 
 	@Override
 	public type setType() {
 		return type.STRING;
 	}
-
+	
 	@Override
 	public byte[] toByteArray() {
 		return null;
+	}
+
+	@Override
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return null;
+	}
+
+	@Override
+	protected byte getTagID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String tagComponent() {
+		return this.str;
 	}
 
 }
