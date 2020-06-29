44040dffea663ded713e02351b03c120ef187fd5
risusan87 <risusan87@localhost>
2020-06-29 12:39:24 -0400

    Finishing off DoubleTag class
    - Added explanation to its constructor
    
    - Enum variable name changed: type -> tagID
    
    - Tag is now null name friendly

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/DoubleTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/DoubleTag.java
@@ -3,8 +3,12 @@ package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 import java.nio.ByteBuffer;
 import java.util.function.Function;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * One of nbt tags. Holds double
+ * 
  * @author risusan87
  *
  */
@@ -12,7 +16,14 @@ public class DoubleTag extends Tag {
 	
 	private double d;
 	
-	public DoubleTag(String par1name, double par2setdouble) {
+	/**
+	 * Creates new tag of double.
+	 * give null as its name to declare as use for list
+	 * 
+	 * @param par1name - tag name
+	 * @param par2setbyte - double to be set
+	 */
+	public DoubleTag(@Nullable String par1name, @Nonnull double par2setdouble) {
 		super(par1name);
 		this.d = par2setdouble;
 	}
@@ -37,6 +48,18 @@ public class DoubleTag extends Tag {
 	@Override
 	public Double tagComponent() {
 		return this.d;
+	}
+
+	@Override
+	protected byte getTagID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <T> T tagComponent() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
