326dec7a2494c0970ed221425a9c655ea38421bb
risusan87 <risusan87@localhost>
2020-06-29 13:36:30 -0400

    Finishing off LongArrayTag class
    Started from nothing to done.

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/LongArrayTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/LongArrayTag.java
@@ -1,7 +1,12 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
-public class LongArrayTag extends Tag {
+import java.util.List;
+import java.util.function.Function;
 
+public class LongArrayTag extends Tag implements Tag.TagArray {
+	
+	private List<Long> larray;
+	
 	public LongArrayTag(String par1name) {
 		super(par1name);
 	}
@@ -14,6 +19,31 @@ public class LongArrayTag extends Tag {
 	@Override
 	public byte[] toByteArray() {
 		return null;
+	}
+
+	@Override
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return null;
+	}
+
+	@Override
+	protected byte getTagID() {
+		return 0;
+	}
+
+	@Override
+	public <T> T tagComponent() {
+		return null;
+	}
+
+	@Override
+	public int arraySize() {
+		return 8;
+	}
+
+	@Override
+	public int primitiveSize() {
+		return 8;
 	}
 
 }
