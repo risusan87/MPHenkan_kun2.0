129556b2db183681f38ab98bd23d3dab42ce79ea
risusan87 <risusan87@localhost>
2020-06-29 12:33:42 -0400

    Finishing off CompoundTag class
    - Added explanation to its constructor
    
    - Made this tag null name friendly

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/CompoundTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/CompoundTag.java
@@ -5,33 +5,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+import javax.annotation.Nullable;
+
 /**
  * One of nbt tags. Holds multiple different types of nbt tag
+ * 
  * @author risusan87
  */
 public class CompoundTag extends Tag implements Tag.TagCompound {
 	
 	private final List<Tag> tags;
 	
-	public CompoundTag(String par1name) {
+	/**
+	 * Creates new tag of compound.
+	 * Compound tag acts like folders that contains multiple different kinds of tag.
+	 * This tag can not be put in a ListTag
+	 * 
+	 * @param par1name - tag name
+	 */
+	public CompoundTag(@Nullable String par1name) {
 		super(par1name);
+		if (this.Tag_name == null || this.Tag_name.equals(""))
+			this.Tag_name = "(unnamed)";
 		this.tags = new ArrayList<Tag>();
 	}
 	
 	@Override
-	public type setType() {
-		return type.COMPOUND;
+	public tagID setType() {
+		return tagID.COMPOUND;
 	}
 	
 	@Override
 	protected Function<Tag, byte[]> _toByteArrayFunction() {
 		return tag -> {
 			ByteBuffer bb = ByteBuffer.allocate(tag.getAllocatedByteSize());
-			bb.put((byte)0x0a)
-			.put(StringTag.toNBTByteTag(this.Tag_name));
+			bb.put(tag.getTagID())
+			.put(StringTag.toNBTByteTag(tag.Tag_name));
 			for (Tag t : ((CompoundTag)tag).tags)
 				bb.put(t.toByteArray());
-			bb.put((byte)0x00);
+			bb.put(new EndTag().toByteArray());
 			return bb.array();
 		};
 	}
@@ -45,6 +57,11 @@ public class CompoundTag extends Tag implements Tag.TagCompound {
 	@Override
 	public synchronized List<Tag> tagComponent() {
 		return this.tags;
+	}
+
+	@Override
+	protected byte getTagID() {
+		return (byte)0x0A;
 	}
 	
 }
