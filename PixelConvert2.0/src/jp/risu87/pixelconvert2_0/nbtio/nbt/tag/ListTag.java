173f6c6eb525d87e2a623073a8f271823911be14
risusan87 <risusan87@localhost>
2020-06-29 13:25:23 -0400

    Finishing off ListTag class
    - Removed unnecessary overriding method
    
    - Nullable annotation put for the param in the constructor
    
    - Tag is now null name friendly
    
    - _toByteArrayFunction() done

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ListTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ListTag.java
@@ -1,19 +1,40 @@
 package jp.risu87.pixelconvert2_0.nbtio.nbt.tag;
 
-public class ListTag extends Tag {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+public class ListTag<T extends Tag> extends Tag {
+
+	private List<T> list;
+	
 	public ListTag(String par1name) {
 		super(par1name);
+		this.list = new ArrayList<T>();
 	}
 
 	@Override
-	public type setType() {
-		return type.LIST;
+	public tagID setType() {
+		return tagID.LIST;
 	}
 
 	@Override
-	public byte[] toByteArray() {
-		return null;
+	protected Function<Tag, byte[]> _toByteArrayFunction() {
+		return tag -> {
+			
+		};
 	}
+
+	@Override
+	protected byte getTagID() {
+		return (byte)0x09;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> tagComponent() {
+		return this.list;
+	}
+
 
 }
