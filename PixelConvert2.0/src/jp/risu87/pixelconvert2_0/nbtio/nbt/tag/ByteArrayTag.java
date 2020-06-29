a2cdeb142b57cf78ac9784df5e06792ad3d2d1f1
risusan87 <risusan87@localhost>
2020-06-29 11:12:41 -0400

    Finishing off ByteArrayTag class
    -Variable name has changed: type -> tagID
    
    -Added explanation in its constructor
    
    -getTagID() method done

--- PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ByteArrayTag.java
+++ PixelConvert2.0/src/jp/risu87/pixelconvert2_0/nbtio/nbt/tag/ByteArrayTag.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * One of nbt tags that holds ByteArray
  * @author risusan87
@@ -13,7 +16,13 @@ public class ByteArrayTag extends Tag implements Tag.TagArray {
 
 	private List<Byte> barray;
 	
-	public ByteArrayTag(String par1name, byte[] par1b) {
+	/**
+	 * Creates new tag of byte array.
+	 * give null as its name to declare as use for list
+	 * @param par1name - tag name
+	 * @param par1b - initial data to be contained
+	 */
+	public ByteArrayTag(@Nullable String par1name, @Nonnull byte[] par1b) {
 		super(par1name);
 		this.barray = new ArrayList<Byte>();
 		for (byte b : par1b)
@@ -21,17 +30,18 @@ public class ByteArrayTag extends Tag implements Tag.TagArray {
 	}
 
 	@Override
-	protected type setType() {
-		return type.BYTE_ARRAY;
+	protected tagID setType() {
+		return tagID.BYTE_ARRAY;
 	}
 	
 	@Override
 	protected Function<Tag, byte[]> _toByteArrayFunction() {
 		return tag -> {
-			ByteBuffer nbt = ByteBuffer.allocate(tag.getAllocatedByteSize());
-			nbt.put((byte)0x07)
-			.put(StringTag.toNBTByteTag(tag.Tag_name))
-			.putInt(barray.size());
+			ByteBuffer nbt = ByteBuffer.allocate(tag.getCorrespondedAllocatedByteSize());
+			if (tag.Tag_name != null)
+				nbt.put((byte)0x07)
+				.put(StringTag.toNBTByteTag(tag.Tag_name));
+			nbt.putInt(barray.size());
 			byte[] b = new byte[barray.size()];
 			for (int i = 0; i < barray.size(); i++)
 				b[i] = barray.get(i);
@@ -54,6 +64,12 @@ public class ByteArrayTag extends Tag implements Tag.TagArray {
 	@Override
 	public int primitiveSize() {
 		return 1;
+	}
+
+	@Override
+	protected byte getTagID() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
